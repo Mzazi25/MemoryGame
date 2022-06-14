9404c872b6f90a63e93f8adc1519bb75bb7f0a63
Caleb Langat <langat.caleb95@gmail.com>
2022-06-14 16:36:17 +0300

    Changed the width and height of each card to be the minumum of cardwidth and cardheight

--- app/src/main/java/com/example/memorygame/MemoryBoardAdapter.kt
+++ app/src/main/java/com/example/memorygame/MemoryBoardAdapter.kt
@@ -4,13 +4,24 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import kotlin.math.min
+
 
 class MemoryBoardAdapter(private val context:Context, private val numPieces: Int)
     : RecyclerView.Adapter<MemoryBoardAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        val cardWidth = parent.width/2
+        val cardHeight = parent.height/4
+        val cardSideLength = min(cardWidth,cardHeight)
+
         val view = LayoutInflater.from(context).inflate(R.layout.memory_card,parent,false)
+        val layoutParams = view.findViewById<CardView>(R.id.cardView).layoutParams
+        layoutParams.width =cardSideLength
+        layoutParams.height =cardSideLength
         return ViewHolder(view)
     }
 
