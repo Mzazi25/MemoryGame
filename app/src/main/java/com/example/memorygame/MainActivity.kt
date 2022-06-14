d118a9d7ebed132648db18916c008ab780e36a58
Caleb Langat <langat.caleb95@gmail.com>
2022-06-14 14:13:34 +0300

    Referenced the widgets created on Main Activity

--- app/src/main/java/com/example/memorygame/MainActivity.kt
+++ app/src/main/java/com/example/memorygame/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.memorygame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 
+private lateinit var rvBoard: RecyclerView
+private lateinit var tvNumPairs : TextView
+private lateinit var tvNumMoves : TextView
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvBoard = findViewById(R.id.rvBoard)
+        tvNumPairs = findViewById(R.id.tvNumPairs)
+        tvNumMoves = findViewById(R.id.tvNumMoves)
     }
 }
