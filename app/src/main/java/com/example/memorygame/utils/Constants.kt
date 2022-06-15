684cf3936dab3a4c0bd3cd14609fbf5fa41cb0a9
Caleb Langat <langat.caleb95@gmail.com>
2022-06-15 12:54:27 +0300

    Added constants for the default icons in a new package and constants file

---
+++ app/src/main/java/com/example/memorygame/utils/Constants.kt
@@ -0,0 +1,20 @@
+package com.example.memorygame.utils
+
+import com.example.memorygame.R
+
+val DEFAULT_ICONS = listOf(
+    R.drawable.ic_bike,
+    R.drawable.ic_face,
+    R.drawable.ic_flag,
+    R.drawable.ic_flight,
+    R.drawable.ic_flower,
+    R.drawable.ic_cup,
+    R.drawable.ic_call,
+    R.drawable.ic_gift,
+    R.drawable.ic_dining,
+    R.drawable.ic_headseat,
+    R.drawable.ic_hand,
+    R.drawable.ic_zoom
+
+
+)
