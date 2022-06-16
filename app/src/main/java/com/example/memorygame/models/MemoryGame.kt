2d238fafa2b1f1a9970006ee844faee310268852
Caleb Langat <langat.caleb95@gmail.com>
2022-06-16 09:47:40 +0300

    Added logic not to flip more than 3 cards at onc

--- app/src/main/java/com/example/memorygame/models/MemoryGame.kt
+++ app/src/main/java/com/example/memorygame/models/MemoryGame.kt
@@ -6,7 +6,8 @@ class MemoryGame (private val boardSize: BoardSize){
 
 
     val cards: List<MemoryCard>
-    val numPairsFound = 0
+    var numPairsFound = 0
+    private var indexOfSingleSelectedCard: Int? =null
 
     init{
         val chosenImages = DEFAULT_ICONS.shuffled().take(boardSize.getNumPairs())
@@ -14,9 +15,41 @@ class MemoryGame (private val boardSize: BoardSize){
         cards = randomizedImages.map { MemoryCard(it, isFaceUp = false, isMatched = false) }
     }
 
-    fun flipCard(position: Int) {
+    fun flipCard(position: Int): Boolean{
         val card =cards[position]
+        var foundMatch = false
+        if (indexOfSingleSelectedCard == null){
+            restoreCards()
+            indexOfSingleSelectedCard= position
+        }else{
+            val foundMatch= checkForMatch(indexOfSingleSelectedCard!! ,position)
+            indexOfSingleSelectedCard=null
+        }
+
         card.isFaceUp = !card.isFaceUp
+        return foundMatch
+    }
+
+    private fun checkForMatch(position1: Int, position2: Int): Boolean {
+        if(cards[position1].identifier != cards[position2].identifier){
+            return false
+
+        }
+        cards[position1].isMatched =true
+        cards[position2].isMatched =true
+        numPairsFound++
+        return true
+
+    }
+
+
+    private fun restoreCards() {
+        for(card in cards){
+            if(!card.isMatched){
+                card.isFaceUp = false
+            }
+
+        }
     }
 
 }
